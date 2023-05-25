ff5be2ab25ccd9b3e762c32cb6929f28ae77b07f
davidgisbey <david.gisbey@digital.cabinet-office.gov.uk>
2023-05-25 10:34:37 +0100

    Add documents tab to featuring index page
    Now that the featurable_editions partial has been refactored to
    work with featurings and features, we can add the documents tab to the
    featuring index page.
    
    This uses the new shared partial and updates the controller to handle
    15 editions per page.

--- app/helpers/admin/topical_event_featurings_helper.rb
+++ app/helpers/admin/topical_event_featurings_helper.rb
@@ -1,8 +1,16 @@
 module Admin::TopicalEventFeaturingsHelper
   def featurable_offsite_links_for_topical_event(featurable_offsite_links, topical_event)
     @featurable_offsite_links_for_topical_event ||= featurable_offsite_links.reject do |link|
-      topical_event.topical_event_featurings.detect do |feature|
-        feature.offsite_link == link
+      topical_event.topical_event_featurings.detect do |featuring|
+        featuring.offsite_link == link
+      end
+    end
+  end
+
+  def featurable_editions_for_topical_event(editions, topical_event)
+    @featurable_editions_for_topical_event ||= editions.reject do |edition|
+      topical_event.topical_event_featurings.detect do |featuring|
+        featuring.edition == edition
       end
     end
   end
